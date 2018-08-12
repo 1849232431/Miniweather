82a463fc0c8eb7ffd5688825f3a5c3455e1ebee4
1849232431 <1849232431@qq.com>
2018-08-12 20:38:23 +0800

    init project

---
+++ app/src/main/java/com/example/hepeng/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hepeng.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hepeng on 8/11/2018.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
